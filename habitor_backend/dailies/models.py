af6c9c37952a81b7cb659fc32ffa70b02c43d7c1
Bhaskar Dutta <bhaskardutta31297@gmail.com>
2022-11-13 23:44:22 +0530

    feat(dailies): add models for daily task and subtask

--- habitor_backend/dailies/models.py
+++ habitor_backend/dailies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class task(models.Model):
+	title = models.TextField()
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	description = models.TextField(blank=True)
+	points = models.PositiveSmallIntegerField(choices = [(0,'TRIVIAL'),(1,'BASIC'),(2,'EASY'),(3,'MEDIUM'),(5,'HARD'),(8,'TROLL')], default=2)
+	
+class subtask(models.Model):
+	title = models.TextField()
+	creator = models.ForeignKey(User, on_delete=models.CASCADE)
+	parent = models.ForeignKey(task, on_delete=models.CASCADE)
+	description = models.TextField(blank=True)
+	points = models.PositiveSmallIntegerField(choices = [(0,'TRIVIAL'),(1,'BASIC'),(2,'EASY'),(3,'MEDIUM'),(5,'HARD'),(8,'TROLL')], default=1)
